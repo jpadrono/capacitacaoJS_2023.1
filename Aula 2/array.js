756a2c3e54c1acb59153ee300751a8dae7fc9784
jpadrono <joaopsmbraga@gmail.com>
2023-05-04 19:06:10 -0300

    Update array.js

--- Aula 2/array.js
+++ Aula 2/array.js
@@ -1,21 +1,45 @@
 /* ARRAY */
 // - multiple types
+//arrays podem ter variáveis de diferentes tipos
+//         number , string, boolean,      object,            number
+let arr = [  1,   'string', true, {nome: 'joão', idade:'20'}, 1900];
+
 // - multi-dimensional array (nesting)
-// - .push(), .pop(), .unshift(), .shift()  
+//podemos ter arrays dentro de arrays
+
+let nesting = [['array', 1], ['array', 2, ['array', 2.1]]];
+// - .push()
+//adiciona o elemento no final da array
+arr.push('final'); 
+console.log(arr)// [  1,   'string', true, {nome: 'joão', idade:'20'}, 1900, 'final']
+
+// - .unshift()
+//adiciona o elemento no início da array
+arr.unshift('inicio');
+console.log(arr)// ['inicio',  1,   'string', true, {nome: 'joão', idade:'20'}, 1900, 'final']
+
+
+
+// - .pop()
+//remove o elemento do final da array e retorna o elemento removido
+let saiu = arr.pop(); //saiu == 'final'
+console.log(arr)//['inicio', 1,'string', true, {nome: 'joão', idade:'20'}, 1900]
+
+
+// - .shift()
+//remove o elemento do início da array e retorna o elemento removido
+let saiuTbm = arr.shift();// saiu == 'início'
+console.log(arr)//[  1,   'string', true, {nome: 'joão', idade:'20'}, 1900]
+
+
 // - spread operator
+
 // - destructuring assignment
 // - .length
 // - .splice(), .slice()
 // - delete, indexOf()
 
 
-//         0     1        2                  3              4
-let arr = [1, 'string', true, {nome: 'joão', idade:'20'}, 1900];
-// arr.push(1900); 
-// adiciona no final
-// arr.unshift(1899); // adiciona no inicial
-// let saiu = arr.pop(); //remove do final
-// let saiuTbm = arr.shift(); // remoce no início
 
 let arrMaior = ['outro elemento',...arr, 'novo elemento'];
 

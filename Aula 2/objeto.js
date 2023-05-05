be44caedb22b114eb192da3b4ddde336616fb479
jpadrono <joaopsmbraga@gmail.com>
2023-05-05 01:15:28 -0300

    .

--- Aula 2/objeto.js
+++ Aula 2/objeto.js
@@ -2,12 +2,25 @@
 let meuObjeto = {
     nome: 'sei não',
     ano: 1900,
+    divertido: false,
 
     falar: function(){
         return 'oi'
     }
 };
 
-let data = 'ano'
+//podemos acessar as propriedades e métodos do meu objeto de 2 formas
+//usando obj.propriedade
 
-console.log(meuObjeto[data])
+console.log(meuObjeto.nome)//'sei não'
+console.log(meuObjeto.ano)//'1900'
+
+//usando colchetes
+
+console.log(meuObjeto['divertido'])//false
+
+//com o colchetes podemos usar variáveis
+
+let variavel = 'divertido';
+
+console.log(meuObjeto[variavel])//false
--- Aula 2/string.js
+++ Aula 2/string.js
@@ -1,19 +1,48 @@
 /* STRING */
 // - começa do 0
+//              01234 ou seja contagem[0] == 'i', contagem[1]== 'n' e assim vai
+let contagem = 'index';
+
 // - Não é mutável (só reatribuído)
+let imutavel = 'elado';
+imutavel[2] = 'r';//não funciona
+console.log(imutavel)//'elado'
+
 // - Concatenação
+// usamos o + para concatenar strings
+let primeiro = 'lembra de ';
+let segundo = 'deixar o espaço';
+console.log(primeiro + segundo)//'lembra de deixar o espaço'
+
 // - Aspas
+//para logar aspas simples devemos usar a string com aspas duplas e vice e versa
+let simples = 'posso escrever "aspas duplas" na minha frase';
+let duplas = "posso escrever 'aspas simples' na minha frase";
+
 // - template literal
-// - .length, indexOf(), lastIndexOf(), .replace()
+//normalmente vamos usar crase
+let crase = `posso escrever "aspas duplas" e 'aspas simples' na minha frase`;
+//com o template literal tbm podemos escrever um código javascript na string
+let x = "meu código javascript";
+let templateString = `posso inserir ${x} na minha frase`;
+
 /* - Em JavaScript, uma string é considerada um objeto primitivo. 
     Isso significa que uma string tem propriedades e métodos associados a ela, 
     embora tecnicamente não seja um objeto.  */
+// - .length, indexOf()
+//semelhantes a array
+let tamanho = "essa string tem tamanho 26";
+console.log(tamanho.length);//26
+console.log(tamanho.indexOf('sa'));//2
+//ao passar uma string ele retorna o index no qual a string começa
 
+//lastIndexOf()
+//ultimo index que aparece 
+console.log(tamanho.lastIndexOf('t'));
+
+//.replace()
+//método para mudar uma parte da string
 let str = 'minha frase criativa';
 
-
-
-
-
-console.log(str.replace('minha', 'nossa'))
+console.log(str.replace('minha', 'nossa'))//nossa frase criativa
 
